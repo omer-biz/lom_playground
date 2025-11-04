2b8ffabb642997130bcfa8cd456d3dc2fa7dac2d
omer <omerabdi@protonmail.com>
2025-11-04 09:55:59 +0300

    chore: install tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import elmPlugin from "vite-plugin-elm";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [elmPlugin()],
+  plugins: [elmPlugin(), tailwindcss()],
 });
